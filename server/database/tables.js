a2ae0cd98b588305ccb51bda5cf4e9605ef22903
brianineza01 <60552218+brianineza01@users.noreply.github.com>
2020-02-20 21:33:05 +0200

    first commit on this branch

--- server/database/tables.js
+++ server/database/tables.js
@@ -7,8 +7,19 @@ export const createtable = () =>{
       lastname varchar(25)  NOT NULL,password varchar(10000)  NOT NULL,
       type varchar(10)  NOT NULL,isadmin boolean NOT NULL);`
    ); 
+   client.query(`
+   CREATE TABLE IF NOT EXISTS accounts(
+   id SERIAL PRIMARY KEY,
+   accountnumber SERIAL NOT NULL UNIQUE,
+   createdon date NOT NULL,
+   owneremail VARCHAR(25) NOT NULL,
+   type VARCHAR(10) NOT NULL,
+   status VARCHAR(10) NOT NULL,
+   balance FLOAT NOT NULL);`
+   ); 
 }
 
  export const truncatetable = () =>{
     client.query('TRUNCATE TABLE users CASCADE'); 
+    client.query('TRUNCATE TABLE accounts CASCADE'); 
 }
