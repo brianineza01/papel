cf6995edefc8bf13f055b07131416885029e6c28
brianineza01 <60552218+brianineza01@users.noreply.github.com>
2021-01-28 19:42:20 +0200

    Fixing minor bugs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 [![Build Status](https://travis-ci.com/brianineza01/papel.svg?branch=Develop)](https://travis-ci.com/brianineza01/papel)   [![Coverage Status](https://coveralls.io/repos/github/brianineza01/papel/badge.svg?branch=ch-config-travis)](https://coveralls.io/github/brianineza01/papel?branch=Develop)
 # papel
+
+Papel is a light-weight core banking application that powers banking operations like account creation, customer deposits, and withdrawals. This app is meant to support a single bank, where users can signup and create bank accounts online, but must visit the branch to withdraw or deposit money.
+
+Visit [link to the API!](https://papel-bank-io.herokuapp.com/documentation) for documentation!
--- server/Server.js
+++ server/Server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { createtable , truncatetable } from "./database/tables";
+import { createtable, truncatetable } from "./database/tables";
 import bodyParser from "body-parser";
 import swaggerUI from 'swagger-ui-express';
 import swaggerDoc from '../swagger.json';
@@ -9,24 +9,39 @@ import accountRoutes from "./Routes/accounts";
 import transactionRoute from "./Routes/transaction"
 import userRoute from './Routes/user'
 
+var os = require("os")
+console.log(os.hostname());
+
 dotenv.config();
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(bodyParser.raw("application/JSON"))
 app.use('/documentation', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use('/auth', authRoutes);
-app.use('/accounts' , accountRoutes)
-app.use('/transactions' , transactionRoute)
-app.use('/user' , userRoute)
+app.use('/accounts', accountRoutes)
+app.use('/transactions', transactionRoute)
+app.use('/user', userRoute)
+app.use('/', (req, res) => {
+    console.log(req.hostname);
+    return res.status(200).send({
+        status: 200,
+        message: `Papel is a light-weight core banking application 
+        that powers banking operations like \n account creation, customer deposits,
+        and withdrawals. This app is meant to support a single bank, \n
+        where users can signup and create bank accounts online, but must visit 
+        the branch to withdraw or deposit money. Please visit the following link for documentation`,
+        data: `${process.env.SITE_URL}/documentation`
+    })
+})
 app.use((req, res) => res.status(404).send({
     status: 404,
     error: 'PAGE NOT FOUND',
-  }));
+}));
 app.listen(process.env.PORT || 3000, () => {
     createtable();
-    if(process.env.NODE_ENV == 'test') {
+    if (process.env.NODE_ENV == 'test') {
         truncatetable();
     }
     console.log(`Server listening on port 3000!`)
